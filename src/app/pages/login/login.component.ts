a71b8004cffbe4203a59a5712150c86a31d733fa
Alexandre Santos <sans-script@github.com>
2024-03-25 18:42:40 -0300

    Aula de Forms

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,13 +3,14 @@ import { IconStockeeLogoBwComponent } from '../../icons/icon-stockee-logo-bw/ico
 import { InputComponent } from '../../shared/input/input.component';
 import { ButtonPrimaryComponent } from '../../shared/button-primary/button-primary.component';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [IconStockeeLogoBwComponent,InputComponent,ButtonPrimaryComponent,FormsModule,ReactiveFormsModule],
+  imports: [IconStockeeLogoBwComponent,InputComponent,ButtonPrimaryComponent,FormsModule,ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -17,12 +18,14 @@ export class LoginComponent {
 
   loginForm!:FormGroup;
   FormData!:any
+  Condition:boolean = false
 
   constructor( private formBuilder:FormBuilder){
 
     this.loginForm = formBuilder.group({
       email:[''],
       senha:[''],
+      user:[''],
     })
 
   }
@@ -30,8 +33,9 @@ export class LoginComponent {
 
 
   onSubmit(){
-    this.FormData = this.loginForm.getRawValue()
-    alert( JSON.stringify(this.FormData))
+    this.Condition = !this.Condition
+    // this.FormData = this.loginForm.getRawValue()
+    // alert( JSON.stringify(this.FormData))
   }
 
 }
